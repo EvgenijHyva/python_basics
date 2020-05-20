18de0bcb72f81702f5bf6cc6a38f151a5375e9ea
Evgeny <evgeny.hyvarinen@gmail.com>
2020-05-20 11:40:24 +0300

    task6 is ready

--- Таск3/6.py
+++ Таск3/6.py
@@ -3,10 +3,20 @@
 # программу должна попадать строка из слов, разделенных пробелом. Каждое слово состоит из латинских букв в нижнем
 # регистре. Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы. Необходимо
 # использовать написанную ранее функцию int_func().
-def int_func(some_text):
+def int_func(some_text: str):
     """
-    Function input: text. Function overwrite words with capitalized first word-letter.
+    Function input: text. Function overwrite words with capitalized first letter.
     :param some_text: text, it should be space separated
     :return: word with Capitalised first letter.
     """
-    some_text = str
+    some_text: list = some_text.split()
+    suggestion = ""
+    for word in some_text:
+        word = word[:1].upper() + word[1:]
+        suggestion += "".join(word) + " "
+    return suggestion
+
+# print(int_func(input("текст\n")))
+# print(int_func("здЭсь находится текст длЯ проверки РаБоТы функции")) # -> проверка текста
+
+
