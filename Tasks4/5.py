e7aee5f9033e6687c15596823c75c65ee71ec95c
Evgeny <evgeny.hyvarinen@gmail.com>
2020-05-23 02:48:21 +0300

    created folder for HW4, Tasks 1-5 are ready

---
+++ Tasks4/5.py
@@ -0,0 +1,13 @@
+# 5. Реализовать формирование списка, используя функцию range() и возможности генератора. В список должны войти
+# четные числа от 100 до 1000 (включая границы). Необходимо получить результат вычисления произведения всех элементов
+# списка. Подсказка: использовать функцию reduce().
+from functools import reduce
+my_list = [num for num in range(100, 1001) if num % 2 == 0]
+
+
+def my_func(var1, var2):
+    """function for number production"""
+    return var1 * var2
+
+
+print(reduce(my_func, my_list))
