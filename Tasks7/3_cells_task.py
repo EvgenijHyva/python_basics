b57aab8c8fb987e831d906428ca12a5c023a9295
Evgeny <evgeny.hyvarinen@gmail.com>
2020-06-04 16:49:07 +0300

    cells task No 3 is ready

--- Tasks7/3_cells_task.py
+++ Tasks7/3_cells_task.py
@@ -15,3 +15,50 @@
 # количество ячеек в ряду — 5. Тогда метод make_order() вернет строку: *****\n*****\n**. Или, количество ячеек клетки
 # равняется 15, количество ячеек в ряду — 5. Тогда метод make_order() вернет строку: *****\n*****\n*****. Подсказка:
 # подробный список операторов для перегрузки доступен по ссылке.
+from math import floor
+
+
+class Cell:
+    def __init__(self, group):
+        self.group = group
+
+    def __str__(self):
+        return f"Cells quantity is: \033[32m\033[1m{self.group}\033[0m"
+
+    def __add__(self, other):
+        self.group = self.group + other.group
+        return f"Result of cells sum: \033[32m\033[1m{self.group}\033[0m"
+
+    def __sub__(self, other):
+        if self.group > other.group:
+            self.group = self.group - other.group
+            return f"Result of cells subtraction: \033[32m\033[1m{self.group}\033[0m"
+        else:
+            raise ValueError("can´t subtract! result < 0, that´s mean all cells are dead")
+
+    def __mul__(self, other):
+        self.group = self.group * other.group
+        return f"Result of cells multiplying: \033[32m\033[1m{self.group}\033[0m"
+
+    def __truediv__(self, other):
+        self.group = round(self.group / other.group)
+        return f"Result of cells dividing: \033[32m\033[1m{self.group}\033[0m"
+
+    def make_order(self, num_el_in_row):
+        res = ["".join("[\033[31mO\033[0m]") for i in range(self.group)]
+        for i in range(floor(self.group / num_el_in_row)):
+            res.pop((i + 1) * num_el_in_row - 1)
+            res.insert((i + 1) * num_el_in_row - 1, "[\033[31mO\033[0m]\n")
+        res = "".join(res)
+        return f"Cells quantity in row: \033[31m\033[1m[{num_el_in_row}]\033[0m.\n" \
+               f"Rows: [\033[36m\033[1m{round(self.group/num_el_in_row)}\033[0m] -> \n{res}\n\n" \
+               f"Total cells value: \033[31m\033[1m[{self.group}]\033[0m, "
+
+cell = Cell(100)
+cell2 = Cell(17)
+print(cell + cell2)
+print(cell / cell2)
+print(cell * cell2)
+print(cell)
+print(cell - cell2)
+print(cell.make_order(8))
