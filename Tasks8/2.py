522f35719f1a737b4034fd5360c4eb87d9eb7719
Evgeny <evgeny.hyvarinen@gmail.com>
2020-06-08 21:17:00 +0300

    task 2 is ready

--- Tasks8/2.py
+++ Tasks8/2.py
@@ -1,3 +1,22 @@
 # 2. Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль. Проверьте его работу на данных,
 # вводимых пользователем. При вводе пользователем нуля в качестве делителя программа должна корректно обработать эту
 # ситуацию и не завершиться с ошибкой.
+
+class MyException(Exception):
+    def __init__(self, txt, num):
+        self.txt = txt
+        self.num = num
+
+
+def divide(a, b):
+    try:
+        if b == 0:
+            raise MyException("Occurred exception, divider:", b)
+        res = a / b
+    except MyException as err:
+        return f"Zero divide not allowed {err}"
+    else:
+        return res
+
+
+print(divide(10, 0))
