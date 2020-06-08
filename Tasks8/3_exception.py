4510cd3326e500ccf38f662b8a7bc56175b9d6e7
Evgeny <evgeny.hyvarinen@gmail.com>
2020-06-08 23:17:20 +0300

    task 3 is ready

--- Tasks8/3_exception.py
+++ Tasks8/3_exception.py
@@ -7,5 +7,31 @@
 # необходимо реализовать проверку типа элемента и вносить его в список, только если введено число. Класс-исключение
 # должен не позволить пользователю ввести текст (не число) и отобразить соответствующее сообщение. При этом работа
 # скрипта не должна завершаться.
+class MyException(Exception):
+    def __init__(self, txt, *args):
+        self.txt = txt
+        self.char = args
 
 
+def my_func():
+    my_list = []
+    stop = None
+    while not stop:
+        user = list(input("Enter integers or '$' for exit\n").split(","))
+        # Check for Exception and stop-symbol generator-searchers:
+        check, stop = [i for i in user if not i.isdigit() and i != "$"], [x for x in user if x == "$"]
+        try:
+            if check:
+                raise MyException("Only numbers or '$'! You have entered:", check)
+        except MyException as err:
+            print(err)
+        else:
+            user = [int(i) for i in user if i.isdigit()]
+            my_list += user
+            if stop:
+                print(f"\033[33mYour´s numbers list is \033[0m:\n{my_list},"
+                      f"\n\033[33mList length: \033[0m{len(my_list)}")
+                break
+
+
+my_func()
